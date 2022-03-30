8be460d94211bf278ffd6c3a744e373281875c86
goops2000 <goops2000@naver.com>
2022-03-31 00:18:53 +0900

    fbase 설정, auth 사용자 확인 useState

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { authService } from "fbase";
+
+import AppRouter from "./Router";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return <AppRouter isLoggedIn={isLoggedIn} />;
+}
+
+export default App;
