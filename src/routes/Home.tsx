d90e10c791ff41f965963e9059d4643957b46763
goops2000 <goops2000@naver.com>
2022-04-01 17:47:23 +0900

    3-2

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { dbService } from "fbase";
+import { collection, addDoc } from "firebase/firestore";
+import React, { useState } from "react";
 
 function Home() {
-  return <span>Home</span>;
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event: any) => {
+    event.preventDefault();
+    const docRdf = await addDoc(collection(dbService, "nweets"), {
+      nweet,
+      createAt: Date.now(),
+    });
+    setNweet("");
+    console.log("Document written with ID :", docRdf.id);
+  };
+  const onChange = (event: any) => {
+    setNweet(event.target.value);
+  };
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          value={nweet}
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" value="Nweet" onClick={onSubmit} />
+      </form>
+    </div>
+  );
 }
-
 export default Home;
