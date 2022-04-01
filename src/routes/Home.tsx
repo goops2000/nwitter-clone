ca4de3fe7dd9a7255c815afc9ce7c9f05bf1a8e8
goops2000 <goops2000@naver.com>
2022-04-02 03:33:15 +0900

    nweet map까지

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,42 @@
 import { dbService } from "fbase";
-import { collection, addDoc } from "firebase/firestore";
-import React, { useState } from "react";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { DocumentData } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
 function Home() {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState<Array<nweetsType>>([]);
+
+  interface nweetsType {
+    data: any;
+    id: string;
+  }
+  const getNweets = async () => {
+    const querySnapshot = await getDocs(collection(dbService, "nweets"));
+    querySnapshot.forEach((doc) => {
+      const dbNweets = {
+        data: doc.data().nweet,
+        id: doc.id,
+      };
+      setNweets((prev) => [dbNweets, ...prev]);
+    });
+    const getNweetsArray = querySnapshot.docs.map((doc) => {
+      return {
+        key: doc.id,
+        data: doc.data().nweet,
+      };
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
   const onSubmit = async (event: any) => {
     event.preventDefault();
     const docRdf = await addDoc(collection(dbService, "nweets"), {
@@ -27,6 +60,11 @@ function Home() {
           onChange={onChange}
         />
         <input type="submit" value="Nweet" onClick={onSubmit} />
+        <h4>
+          {nweets.map((dbNweets) => {
+            return <div key={dbNweets.id}>{dbNweets.data}</div>;
+          })}
+        </h4>
       </form>
     </div>
   );
